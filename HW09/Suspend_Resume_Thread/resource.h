794bd2c6784bddd220250c69052ad9a1a9bb87c3
Dima Babich <dimavernigor7@gmail.com>
2024-02-19 11:52:07 +0200

    Upload HW09

---
+++ HW09/Suspend_Resume_Thread/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Suspend_Resume_Thread.rc
+//
+#define IDD_DIALOG1                     101
+#define IDC_PLAY1                       1001
+#define IDC_PLAY2                       1002
+#define IDC_PLAY3                       1003
+#define IDC_STOP1                       1007
+#define IDC_STOP2                       1008
+#define IDC_STOP3                       1009
+#define IDC_STATICINFO1                 1010
+#define IDC_STATICINFO2                 1011
+#define IDC_STATICINFO3                 1012
+#define IDC_STATIC1                     1013
+#define IDC_STATIC2                     1014
+#define IDC_STATIC3                     1015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
